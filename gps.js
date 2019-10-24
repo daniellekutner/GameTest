c41e1ca45197357485b508c4934419b1e920dea2
aturc <adrianturcato@gmail.com>
2019-10-24 19:17:57 -0400

    save location

--- gps.js
+++ gps.js
@@ -1,3 +1,5 @@
+var text = "latitude, longitude \n";
+
 function initGeolocation() {
   console.log("fn initGeolocation")
   if (navigator && navigator.geolocation) {
@@ -11,6 +13,11 @@ function errorCallback() {}
  
 function successCallback(position) {
   console.log("fn: successCallback",position)
+
+  text += `${position.coords.latitude}, ${position.coords.longitude} \n`;
+  document.getElementById("coords").innerHTML += `latitude: ${position.coords.latitude}, longitude: ${position.coords.longitude} <br>`;
+  console.log(text)
+
   var blob = new Blob([position], { type: "text/plain;charset=utf-8" });
   saveAs(blob, "dynamic.txt");
 }
