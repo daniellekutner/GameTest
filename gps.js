8d415ccd1cda7281508201788cf6144bd26d7ac4
aturc <adrianturcato@gmail.com>
2019-10-24 19:04:18 -0400

    get position

--- gps.js
+++ gps.js
@@ -1,5 +1,5 @@
 function initGeolocation() {
-  
+  console.log("fn initGeolocation")
   if (navigator && navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
   } else {
@@ -10,11 +10,5 @@ function initGeolocation() {
 function errorCallback() {}
  
 function successCallback(position) {
-  
-  var mapUrl = "http://maps.google.com/maps/api/staticmap?center=";
-  mapUrl = mapUrl + position.coords.latitude + ',' + position.coords.longitude;
-  mapUrl = mapUrl + '&zoom=15&size=512x512&maptype=roadmap&sensor=false';
-  var imgElement = document.getElementById("static-map");
-  imgElement.src = mapUrl;
-  alert("hi:",position.coords.latitude,position.coords.longitude)
+  console.log("fn: successCallback",position)
 }
